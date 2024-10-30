d22d4af0a6df1a57f60236753226edb1a5f238f1
Daseanle <daseanleelee@gmail.com>
2024-10-30 12:37:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '贷大师企业咨询', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '贷大师企业咨询，欢迎关注', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://dasean.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '企业融资咨询, 企业贷款,贷款,金融,企业银行贷款,银行贷款,企业如何从银行贷款,银行信用贷款,企业信用贷款', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
