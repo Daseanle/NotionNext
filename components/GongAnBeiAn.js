4534716fe51bc919368a27888f032f1efaef9b5b
Daseanle <daseanleelee@gmail.com>
2024-10-31 06:17:43 +0800

    Update GongAnBeiAn.js

--- components/GongAnBeiAn.js
+++ components/GongAnBeiAn.js
@@ -1,4 +1,3 @@
-import { siteConfig } from '@/lib/config'
 import LazyImage from './LazyImage'
 
 /**
@@ -6,25 +5,17 @@ import LazyImage from './LazyImage'
  * @returns
  */
 export const GongAnBeiAn = () => {
-  const BEI_AN_GONGAN = siteConfig(
-    'BEI_AN_GONGAN',
-    '皖公网安备34060002050051号'
-  )
-  // 从BEI_AN_GONGAN 字段中利用正则匹配提取出纯数字部分
-  const codeMatch = BEI_AN_GONGAN.match(/\d+/) // 匹配纯数字
-  const code = codeMatch ? codeMatch[0] : null // 如果匹配成功则取出数字部分
+  // const BEI_AN_GONGAN = siteConfig(
+  //   'BEI_AN_GONGAN',
+  //   '皖公网安备34060002050051号'
+  // )
+  // const codeMatch = BEI_AN_GONGAN.match(/\d+/)
+  // const code = codeMatch ? codeMatch[0] : null
 
-  const href = `https://beian.mps.gov.cn/#/query/webSearch?code=${code}` // 动态生成链接
+  // const href = `https://beian.mps.gov.cn/#/query/webSearch?code=${code}`
 
-  if (!BEI_AN_GONGAN) {
-    return null
-  }
-  return (
-    <div className='flex flex-nowrap items-center gap-1'>
-      <LazyImage src='/images/gongan.png' width={15} height={15} />
-      <a href={href} target='_blank' rel='noopener noreferrer'>
-        {BEI_AN_GONGAN}
-      </a>
-    </div>
-  )
+  // if (!BEI_AN_GONGAN) {
+  //   return null
+  // }
+  return null; // 返回null，不显示备案号
 }
