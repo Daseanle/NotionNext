fa2d7fe13cd0844922830448ccc3fd13d062ba13
Daseanle <daseanleelee@gmail.com>
2024-10-31 09:34:32 +0800

    Update GongAnBeiAn.js

--- components/GongAnBeiAn.js
+++ components/GongAnBeiAn.js
@@ -1,21 +1,11 @@
-import LazyImage from './LazyImage'
+import { siteConfig } from '@/lib/config';
+import LazyImage from './LazyImage';
 
 /**
  * 公安备案号组件
  * @returns
  */
 export const GongAnBeiAn = () => {
-  // const BEI_AN_GONGAN = siteConfig(
-  //   'BEI_AN_GONGAN',
-  //   '皖公网安备34060002050051号'
-  // )
-  // const codeMatch = BEI_AN_GONGAN.match(/\d+/)
-  // const code = codeMatch ? codeMatch[0] : null
-
-  // const href = `https://beian.mps.gov.cn/#/query/webSearch?code=${code}`
-
-  // if (!BEI_AN_GONGAN) {
-  //   return null
-  // }
-  return null; // 返回null，不显示备案号
-}
+  // 直接返回 null，不渲染任何内容
+  return null;
+};
